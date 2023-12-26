d37e07973524a8da05f7d0f0c15e9d1d66d9d048
AlbertJQM <albertjhon.128@gmail.com>
2023-12-26 10:25:59 -0400

    Funcionalidad "editar"

--- src/app/modules/shared/components/modal-formulario/modal-formulario.component.ts
+++ src/app/modules/shared/components/modal-formulario/modal-formulario.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
 import { event } from 'jquery';
+import { Instructivo } from 'src/app/models/instructivos';
 import { DataServiceService } from 'src/app/services/data-service.service';
 import { DataSharedService } from 'src/app/services/data-shared.service';
 
@@ -17,16 +18,26 @@ export class ModalFormularioComponent  implements OnInit {
   tipo: string = "";
   archivo!: File;
   fechaInicio!: string; 
+  instructivo!: Instructivo;
 
+  accion = "Agregar";
 
-
-  constructor(private datosService: DataServiceService, private modalController: ModalController, private navParams: NavParams, private serviceScript: DataSharedService) { }
+  constructor(private datosService: DataServiceService, private modalController: ModalController, private navParams: NavParams, private serviceScript: DataSharedService) { 
+    this.instructivo = this.navParams.get('instructivo');  
+    console.log(this.instructivo);
+  }
 
   ngOnInit() {
     this.serviceScript.loadScript('my-script', 'src/app/utils/lib/custom-input.js')
             .then(data => {
                 console.log('script loaded ', data);
             }).catch(error => console.log(error));
+    this.esEditar();
+  }
+  esEditar(){
+    if(this.instructivo !== undefined){
+      this.accion = "Editar";
+    }
   }
 
   enviarDatos(){
